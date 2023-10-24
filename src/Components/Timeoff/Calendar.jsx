08954caf63d5eed8d6699d6106887fd9212ed6bf
iceror <ilsecervor@gmail.com>
2023-10-24 10:33:38 -0600

    create structure

---
+++ src/Components/Timeoff/Calendar.jsx
@@ -0,0 +1,39 @@
+export const Calendar = () => {
+  return (
+    <>
+      <section className='holiday'>
+        <h2> X </h2> {/* función para traer los días disponibles*/}
+        <h3>Días disponibles</h3>
+        <hr />
+        <div>
+          <p>Saldo de arrastre de vacaciones</p>
+          <p>8</p>
+        </div>
+        <div>
+          <p>Días de vacaciones 2023</p>
+          <p>15</p>
+        </div>
+        <div>
+          <p>Saldo total de vacaicones</p>
+          <p>23</p>
+        </div>
+        <hr />
+        <div>
+          <p>Permisos por año</p>
+          <p>2</p>
+        </div>
+        <div>
+          <p>Permisos utilizados</p>
+          <p>2</p>
+        </div>
+        <div>
+          <p>Saldo de ausencias</p>
+          <p>2</p>
+        </div>
+      </section>
+      <section className='calendar'>
+
+      </section>
+    </>
+  )
+}
