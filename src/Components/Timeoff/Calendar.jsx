5c7262c18e195d0cb748f2dc2602e2f8ec4d76fc
iceror <ilsecervor@gmail.com>
2023-10-24 15:11:56 -0600

    update calendar

--- src/Components/Timeoff/Calendar.jsx
+++ src/Components/Timeoff/Calendar.jsx
@@ -1,41 +1,59 @@
-export const Calendar = () => {
+import {
+  Calendar as BigCalendar,
+  momentLocalizer,
+  Views,
+  // View,
+} from "react-big-calendar";
+import moment from "moment";
+import YearView from "./YearView";
+import { useState } from "react";
+const localizer = momentLocalizer(moment);
+const events = [
+  {
+    start: moment("2023-10-10T10:00:00").toDate(),
+    end: moment("2023-10-10T12:00:00").toDate(),
+    title: "MRI Registration",
+  },
+  {
+    start: moment("2023-01-10T10:00:00").toDate(),
+    end: moment("2023-01-10T11:00:00").toDate(),
+    title: "ENT Appointment",
+  },
+  {
+    start: moment("2023-06-10T10:00:00").toDate(),
+    end: moment("2023-06-10T11:00:00").toDate(),
+    title: "Plastic Surgery",
+  },
+];
+
+
+export default function Calendar({theme}) {
+  console.log(theme);
+  const [view, setView] = useState(Views.YEAR); // Set the default view to YEAR
+  const [date, setDate] = useState(new Date());
+  const views = {
+    month: true,
+    year: YearView,
+  };
+  
+  const handleViewChange = (newView) => {
+    if (views[newView]) {
+      setView(newView);
+    }
+  };
+  
   return (
-    <>
-      <section className='holiday'>
-        <h2> X </h2> {/* función para traer los días disponibles*/}
-        <h3>Días disponibles</h3>
-        <hr />
-        <div>
-          <p>Saldo de arrastre de vacaciones</p>
-          <p>8</p>
-        </div>
-        <div>
-          <p>Días de vacaciones 2023</p>
-          <p>15</p>
-        </div>
-        <div>
-          <p>Saldo total de vacaicones</p>
-          <p>23</p>
-        </div>
-        <hr />
-        <div>
-          <p>Permisos por año</p>
-          <p>2</p>
-        </div>
-        <div>
-          <p>Permisos utilizados</p>
-          <p>2</p>
-        </div>
-        <div>
-          <p>Saldo de ausencias</p>
-          <p>2</p>
-        </div>
-      </section>
-      <section className='calendar'>
-
-      </section>
-    </>
-  )
+    <BigCalendar
+      localizer={localizer}
+      defaultView={Views.YEAR} // Set the default view to YEAR
+      view={view}
+      date={date}
+      onView={handleViewChange}
+      onNavigate={(newDate) => setDate(newDate)}
+      events={events}
+      views={views}
+      messages={{ year: "Year" }}
+      theme = {theme}
+    />
+  );
 }
-
-export default Calendar
