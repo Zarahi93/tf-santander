427e05540a055df255e65c504eb67ca296849de2
KarlaMacedo <karlapaolamacedo@gmail.com>
2023-10-24 19:42:15 -0600

    Fixed select date

--- src/Components/Timeoff/Form.jsx
+++ src/Components/Timeoff/Form.jsx
@@ -12,6 +12,8 @@ function TimeOffForm({ onCancel }) {
   const [toDate, setToDate] = useState('');
   const [description, setDescription] = useState('');
   const [showWarning, setShowWarning] = useState(false);
+  const [showWarning2, setShowWarning2] = useState(false);
+  const [showWarning3, setShowWarning3] = useState(false);
   const [isImageModalOpen, setImageModalOpen] = useState(false);
 
   const handleReasonChange = (event) => {
@@ -19,11 +21,41 @@ function TimeOffForm({ onCancel }) {
   };
 
   const handleFromDateChange = (event) => {
-    setFromDate(event.target.value);
+    const selectedDate = new Date(event.target.value);
+    const toSelectDate = new Date(toDate);
+    if (selectedDate !== toSelectDate) {
+      if (toSelectDate && selectedDate > toSelectDate) {
+        setShowWarning3(true);
+      } else {
+        setShowWarning2(false);
+        setShowWarning3(false);
+        setFromDate(event.target.value);
+      }
+    } else {
+      setFromDate(event.target.value);
+    }
   };
 
   const handleToDateChange = (event) => {
-    setToDate(event.target.value);
+    const selectedDate = new Date(event.target.value);
+    const today = new Date();
+    const yesterday = new Date(today);
+    yesterday.setDate(today.getDate() - 2);
+    const fromSelectDate = new Date(fromDate);
+
+    if (selectedDate !== fromSelectDate) {
+      if (fromSelectDate && selectedDate < fromSelectDate) {
+        setShowWarning3(true);
+      } else if (selectedDate < yesterday) {
+        setShowWarning2(true);
+      } else {
+        setShowWarning2(false);
+        setShowWarning3(false);
+        setToDate(event.target.value);
+      }
+    } else {
+      setFromDate(event.target.value);
+    }
   };
 
   const handleDescriptionChange = (event) => {
@@ -57,6 +89,13 @@ function TimeOffForm({ onCancel }) {
     }
   };
 
+  function getFormattedDate(date) {
+    const year = date.getFullYear();
+    const month = `${date.getMonth() + 1}`.padStart(2, '0');
+    const day = `${date.getDate()}`.padStart(2, '0');
+    return `${year}-${month}-${day}`;
+  }
+
   return (
     <Container maxWidth="lg"
       style={{
@@ -68,6 +107,16 @@ function TimeOffForm({ onCancel }) {
       {showWarning && (
         <Typography variant="body1" align="center" color="error" style={{ fontSize: "18px", marginBottom: "25px" }}>
           Completa todos los campos requeridos.
+        </Typography>
+      )}
+      {showWarning2 && (
+        <Typography variant="body1" align="center" color="error" style={{ fontSize: "18px", marginBottom: "25px" }}>
+          Selecciona una fecha posterior al día de hoy.
+        </Typography>
+      )}
+      {showWarning3 && (
+        <Typography variant="body1" align="center" color="error" style={{ fontSize: "18px", marginBottom: "25px" }}>
+          Selecciona una fecha posterior al día de inicio.
         </Typography>
       )}
       <form onSubmit={handleSubmit}>
@@ -106,6 +155,9 @@ function TimeOffForm({ onCancel }) {
               type="date"
               value={fromDate}
               onChange={handleFromDateChange}
+              inputProps={{
+                min: getFormattedDate(new Date()),
+              }}
               style={{
                 backgroundColor: theme.palette.bgColor.main,
               }}
@@ -119,13 +171,14 @@ function TimeOffForm({ onCancel }) {
               type="date"
               value={toDate}
               onChange={handleToDateChange}
+              inputProps={{ min: fromDate }}
               style={{
                 backgroundColor: theme.palette.bgColor.main,
               }}
             />
           </Grid>
           <Grid item xs={12}>
-            <Typography onClick={openImageModal} style={{color:"blue", cursor:"pointer", textDecoration:"underline"}}>Información sobre permisos especiales</Typography>
+            <Typography onClick={openImageModal} style={{ color: "blue", cursor: "pointer", textDecoration: "underline" }}>Información sobre permisos especiales</Typography>
           </Grid>
           <Grid item xs={12}>
             <TextField
@@ -154,7 +207,7 @@ function TimeOffForm({ onCancel }) {
         </Grid>
       </form>
       <Dialog open={isImageModalOpen} onClose={closeImageModal}>
-      <DialogTitle>
+        <DialogTitle>
           <IconButton
             color="inherit"
             onClick={closeImageModal}
@@ -164,7 +217,7 @@ function TimeOffForm({ onCancel }) {
           </IconButton>
         </DialogTitle>
         <DialogContent>
-          <img src={permitsTable} alt="Imagen información permisos especiales" style={{maxWidth:"-webkit-fill-available"}}/>
+          <img src={permitsTable} alt="Imagen información permisos especiales" style={{ maxWidth: "-webkit-fill-available" }} />
         </DialogContent>
         <DialogActions>
           <Button variant="contained" onClick={closeImageModal} style={{ backgroundColor: "red" }}>
