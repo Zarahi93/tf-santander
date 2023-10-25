757c0f97bb065f83b04985112956fb2073b010aa
iceror <ilsecervor@gmail.com>
2023-10-25 10:45:51 -0600

    update calendar

--- src/Components/Timeoff/Calendar.jsx
+++ src/Components/Timeoff/Calendar.jsx
@@ -8,28 +8,31 @@ import moment from "moment";
 import YearView from "./YearView";
 import { useState } from "react";
 const localizer = momentLocalizer(moment);
-const events = [
-  {
-    start: moment("2023-10-10T10:00:00").toDate(),
-    end: moment("2023-10-10T12:00:00").toDate(),
-    title: "MRI Registration",
-  },
-  {
-    start: moment("2023-01-10T10:00:00").toDate(),
-    end: moment("2023-01-10T11:00:00").toDate(),
-    title: "ENT Appointment",
-  },
-  {
-    start: moment("2023-06-10T10:00:00").toDate(),
-    end: moment("2023-06-10T11:00:00").toDate(),
-    title: "Plastic Surgery",
-  },
-];
+// const nationalFestivities = [
+//   // feriados que NO se
+//   //feriado nacional que sí se trabaja
+//   //resalte las vacaciones
+//   {
+//     start: moment("2023-09-15T10:00:00").toDate(),
+//     end: moment("2023-09-15T12:00:00").toDate(),
+//     // title: "Independencia de México",
+//   },
+//   {
+//     start: moment("2023-03-21T10:00:00").toDate(),
+//     end: moment("2023-03-21T11:00:00").toDate(),
+//     // title: "Natalicio Benito Juárez",
+//   },
+//   {
+//     start: moment("2023-06-10T10:00:00").toDate(),
+//     end: moment("2023-06-10T11:00:00").toDate(),
+//     // title: "Plastic Surgery",
+//   },
+// ];
 
 
 export default function Calendar({theme}) {
-  console.log(theme);
-  const [view, setView] = useState(Views.YEAR); // Set the default view to YEAR
+  // console.log(theme);
+  const [view, setView] = useState(Views.MONTH); // Set the default view to YEAR
   const [date, setDate] = useState(new Date());
   const views = {
     month: true,
@@ -45,12 +48,11 @@ export default function Calendar({theme}) {
   return (
     <BigCalendar
       localizer={localizer}
-      defaultView={Views.YEAR} // Set the default view to YEAR
+      defaultView={Views.MONTH}
       view={view}
       date={date}
       onView={handleViewChange}
       onNavigate={(newDate) => setDate(newDate)}
-      events={events}
       views={views}
       messages={{ year: "Year" }}
       theme = {theme}
